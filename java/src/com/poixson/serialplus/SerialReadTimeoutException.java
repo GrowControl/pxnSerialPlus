e45af09e5f908fda508c3f6f9cd964bd355398a0
lorenzop <lorenzo@poixson.com>
2017-09-08 03:07:00 -0400

    added SerialReadTimeoutException

---
+++ java/src/com/poixson/serialplus/SerialReadTimeoutException.java
@@ -0,0 +1,52 @@
+package com.poixson.serialplus;
+
+import com.poixson.utils.StringUtils;
+
+public class SerialReadTimeoutException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public final String portName;
+	public final String methodName;
+	public final long   timeout;
+	public final int    size;
+
+
+
+	public SerialReadTimeoutException(final String portName,
+			final String methodName, final long timeout, final int size) {
+		super(
+			(new StringBuilder())
+				.append("Serial read timeout port: ")
+				.append(portName)
+				.append(" method: ")
+				.append(StringUtils.ForceEnds("()", methodName))
+				.append(" timeout: ")
+				.append(timeout)
+				.append(" size: ")
+				.append(size)
+				.toString()
+		);
+		this.portName   = portName;
+		this.methodName = methodName;
+		this.timeout    = timeout;
+		this.size       = size;
+	}
+
+
+
+	public String getPortName() {
+		return this.portName;
+	}
+	public String getMethodName() {
+		return this.methodName;
+	}
+	public long getTimeout() {
+		return this.timeout;
+	}
+	public int getByteSize() {
+		return this.size;
+	}
+
+
+
+}

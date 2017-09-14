263958f852fca7427e211e0d5334609fbfa6acde
lorenzop <lorenzo@poixson.com>
2017-09-14 00:08:10 -0400

    added empty NativeD2xx.c and NativeCh34x.c files

---
+++ natives/NativeCh34x.c
@@ -0,0 +1,15 @@
+/* PoiXson SerialPlus 1.x
+ * copyright 2017
+ * license GPL-3
+ * lorenzo at poixson.com
+ * http://poixson.com/
+ */
+
+#include <jni.h>
+
+
+
+
+
+
+
---
+++ natives/NativeD2xx.c
@@ -0,0 +1,27 @@
+/* PoiXson SerialPlus 1.x
+ * copyright 2017
+ * license GPL-3
+ * lorenzo at poixson.com
+ * http://poixson.com/
+ */
+
+#include <jni.h>
+
+
+//#include "ftd2xx.h"
+
+
+static char* vendor_ids[] = {
+	"0403"
+};
+
+static char* product_ids[] = {
+	"6001"
+};
+
+
+
+
+
+
+

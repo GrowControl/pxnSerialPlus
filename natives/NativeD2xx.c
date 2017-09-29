96e3a1ec03e0501844abbeb038437ac39b3e19e9
lorenzop <lorenzo@poixson.com>
2017-09-29 00:48:39 -0400

    added empty functions to d2xx and ch34x .c files

--- natives/NativeD2xx.c
+++ natives/NativeD2xx.c
@@ -31,4 +31,90 @@ return 0;
 
 
 
+// natOpenPort(port-name)
+JNIEXPORT jlong JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natOpenPort
+(JNIEnv *env, jobject obj, jstring portName) {
+return 0;
+}
 
+
+
+// natClosePort(handle)
+JNIEXPORT jboolean JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natClosePort
+(JNIEnv *env, jobject obj, jlong handle) {
+return JNI_FALSE;
+}
+
+
+
+// natSetParams(handle, baud, byte-size, stop-bits, parity, flags)
+JNIEXPORT jlong JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natSetParams
+(JNIEnv *env, jobject obj, jlong handle, jint baud,
+jint byteSize, jint stopBits, jint parity, jint flags) {
+return 0;
+}
+
+
+
+// natSetBlocking(handle, blocking) - blocking/non-blocking
+JNIEXPORT jlong JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natSetBlocking
+(JNIEnv *env, jobject obj, jlong handle, jboolean blocking) {
+return 0;
+}
+
+
+
+// natSetVMinVTime(handle, vMin, vTime)
+JNIEXPORT jlong JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natSetVMinVTime
+(JNIEnv *env, jobject obj, jlong handle, jint vMin, jint vTime) {
+return 0;
+}
+
+
+
+// natSetLineStatus(handle, rts, dtr)
+JNIEXPORT jlong JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natSetLineStatus
+(JNIEnv *env, jobject obj,
+jlong handle, jboolean setRTS, jboolean setDTR) {
+return 0;
+}
+
+
+
+// natGetInputBytesCount(handle)
+JNIEXPORT jint JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natGetInputBytesCount
+(JNIEnv *env, jobject obj, jlong handle) {
+return 0;
+}
+// natGetOutputBytesCount(handle)
+JNIEXPORT jint JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natGetOutputBytesCount
+(JNIEnv *env, jobject obj, jlong handle) {
+return 0;
+}
+
+
+
+// natReadBytes(handle, bytes, length)
+JNIEXPORT jint JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natReadBytes
+(JNIEnv *env, jobject obj,
+jlong handle, jbyteArray bytes, jint length) {
+return 0;
+}
+
+
+
+// natWriteBytes(handle, bytes)
+JNIEXPORT jlong JNICALL
+Java_com_poixson_serialplus_natives_NativeD2xx_natWriteBytes
+(JNIEnv *env, jobject obj, jlong handle, jbyteArray bytes) {
+return 0;
+}

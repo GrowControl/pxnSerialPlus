864c2a4c5d4b6c52fb33896be96b796a7e9ace53
lorenzop <lorenzo@poixson.com>
2017-09-29 00:29:24 -0400

    cleaned up and finished SerialPlusFactory class

--- java/src/com/poixson/serialplus/SerialPlusFactory.java
+++ java/src/com/poixson/serialplus/SerialPlusFactory.java
@@ -6,39 +6,75 @@ import com.poixson.serialplus.enums.Baud;
 import com.poixson.serialplus.enums.DataBits;
 import com.poixson.serialplus.enums.Parity;
 import com.poixson.serialplus.enums.StopBits;
+import com.poixson.utils.ErrorMode;
 import com.poixson.utils.Utils;
+import com.poixson.utils.xTime;
 
 
 public class SerialPlusFactory {
 
+	public static final ErrorMode DEFAULT_ERROR_MODE = SerialPlus.DEFAULT_ERROR_MODE;
+	private volatile ErrorMode errorMode = null;
+
+	public static final long DEFAULT_READ_TIMEOUT  = 1000L;
+	public static final long DEFAULT_READ_INTERVAL = 100L;
+
 	private String   portName = null;
-	private Baud     baud     = Baud.B9600;
-	private DataBits byteSize = DataBits.DATA_BITS_8;
-	private StopBits stopBits = StopBits.STOP_BITS_1;
-	private Parity   parity   = Parity.PARITY_NONE;
-	private boolean  rts      = false;
-	private boolean  dtr      = false;
-	private int      flags    = 0;
-	private boolean  blocking = true;
+	private Baud     baud     = null;
 
-//	private xTime readTimeout  = xTime.get();
-//	private xTime readInterval = xTime.get();
+	private DataBits byteSize = null;
+	private StopBits stopBits = null;
+	private Parity   parity   = null;
+
+	private Boolean  rts      = null;
+	private Boolean  dtr      = null;
+	private Integer  flags    = null;
+
+	private final xTime readTimeout  = xTime.get();
+	private final xTime readInterval = xTime.get();
 
 
 
-	public SerialPlusFactory() {}
+	public static SerialPlusFactory get() {
+		return new SerialPlusFactory();
+	}
+	public static SerialPlusFactory get(final String portName) {
+		return new SerialPlusFactory(portName);
+	}
+	public static SerialPlusFactory get(final String portName, final int baud) {
+		return new SerialPlusFactory(portName, baud);
+	}
+	public static SerialPlusFactory get(final String portName, final Baud baud) {
+		return new SerialPlusFactory(portName, baud);
+	}
+
+
+
+	public SerialPlusFactory() {
+	}
+	public SerialPlusFactory(final String portName) {
+		this();
+		this.setPortName(portName);
+	}
+	public SerialPlusFactory(final String portName, final int baud) {
+		this();
+		this.setPortName(portName);
+		this.setBaud(baud);
+	}
+	public SerialPlusFactory(final String portName, final Baud baud) {
+		this();
+		this.setPortName(portName);
+		this.setBaud(baud);
+	}
 
 
 
 	public SerialPlus build() {
-		if (Utils.isBlank(this.portName)) throw new InvalidParameterException("portName is required");
-		if (this.baud == null)            throw new InvalidParameterException("baud is required");
-		if (this.byteSize == null)        throw new InvalidParameterException("byteSize is required");
-		if (this.stopBits == null)        throw new InvalidParameterException("stopBits is required");
-		if (this.parity == null)          throw new InvalidParameterException("parity is required");
-		final SerialPlus serial =
-			new SerialPlus(
-				this.portName,
+		final String portName = this.portName;
+		if (Utils.isBlank(portName)) throw new InvalidParameterException("portName is required");
+		final DeviceConfig cfg =
+			new DeviceConfig(
+				portName,
 				this.baud,
 				this.byteSize,
 				this.stopBits,
@@ -46,9 +82,26 @@ public class SerialPlusFactory {
 				this.rts,
 				this.dtr,
 				this.flags,
-				this.blocking
+				this.readTimeout,
+				this.readInterval
 			);
-		return serial;
+		return new SerialPlus(cfg);
+	}
+
+
+
+	// error mode
+	public ErrorMode getErrorMode() {
+		final ErrorMode mode = this.errorMode;
+		return (
+			mode == null
+			? DEFAULT_ERROR_MODE
+			: mode
+		);
+	}
+	public SerialPlusFactory setErrorMode(final ErrorMode mode) {
+		this.errorMode = mode;
+		return this;
 	}
 
 
@@ -67,7 +120,7 @@ public class SerialPlusFactory {
 		return this;
 	}
 	public SerialPlusFactory setBaud(final int value) {
-		final Baud baud = Baud.fromInt(value);
+		final Baud baud = Baud.FromInt(value);
 		if (baud == null) throw new InvalidParameterException("Invalid baud: "+Integer.toString(value));
 		return this.setBaud(baud);
 	}
@@ -80,11 +133,10 @@ public class SerialPlusFactory {
 		return this;
 	}
 	public SerialPlusFactory setByteSize(final int value) {
-		final DataBits bits = DataBits.fromInt(value);
+		final DataBits bits = DataBits.FromInt(value);
 		if (bits == null) throw new InvalidParameterException("Invalid bit size: "+Integer.toString(value));
 		return this.setByteSize(bits);
 	}
-
 
 
 
@@ -94,12 +146,12 @@ public class SerialPlusFactory {
 		return this;
 	}
 	public SerialPlusFactory setStopBits(final String value) {
-		final StopBits bits = StopBits.fromString(value);
+		final StopBits bits = StopBits.FromString(value);
 		if (bits == null) throw new InvalidParameterException("Invalid stop bits: "+value);
 		return this.setStopBits(bits);
 	}
 	public SerialPlusFactory setStopBits(final int value) {
-		final StopBits bits = StopBits.fromInt(value);
+		final StopBits bits = StopBits.FromInt(value);
 		if (bits == null) throw new InvalidParameterException("Invalid stop bits: "+Integer.toString(value));
 		return this.setStopBits(bits);
 	}
@@ -112,7 +164,7 @@ public class SerialPlusFactory {
 		return this;
 	}
 	public SerialPlusFactory setParity(final String value) {
-		final Parity parity = Parity.fromString(value);
+		final Parity parity = Parity.FromString(value);
 		if (parity == null) throw new InvalidParameterException("Invalid parity: "+value);
 		return this.setParity(parity);
 	}
@@ -121,11 +173,11 @@ public class SerialPlusFactory {
 
 	// line status
 	public SerialPlusFactory setRTS(final boolean value) {
-		this.rts = value;
+		this.rts = Boolean.valueOf(value);
 		return this;
 	}
 	public SerialPlusFactory setDTR(final boolean value) {
-		this.dtr = value;
+		this.dtr = Boolean.valueOf(value);
 		return this;
 	}
 
@@ -133,7 +185,7 @@ public class SerialPlusFactory {
 
 	// flags
 	public SerialPlusFactory setFlags(final int flags) {
-		this.flags = flags;
+		this.flags = Integer.valueOf(flags);
 		return this;
 	}
 
